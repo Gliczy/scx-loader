6635de8b85798d59449e163fe33ecaf81f73fa78
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2025-11-02 04:02:11 +0400

    scx_loader: suppress clippy warnings
    clippy::unused_self breaks zbus interface
    clippy::cast_possible_wrap child.id returns universal u32,
    but nix crate requires i32

--- crates/scx_loader/src/main.rs
+++ crates/scx_loader/src/main.rs
@@ -5,6 +5,9 @@
 // This software may be used and distributed according to the terms of the
 // GNU General Public License version 2.
 
+#![allow(clippy::unused_self)]
+#![allow(clippy::cast_possible_wrap)]
+
 mod logger;
 
 use scx_loader::dbus::LoaderClientProxy;
